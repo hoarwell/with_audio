f952327edfeef0b43901b4da743c65a77cd1dc71
youn9jo <youn9jo@gmail.com>
2021-06-24 22:41:01 +0900

    21.06.24. 22:40 img hover 계속 커지기 및 위에 화면 커서 대면 오디오 컨트롤러 내려오게하기, 마우스 커서 모양 바꿔봄

--- src/components/WorkDetail.js
+++ src/components/WorkDetail.js
@@ -8,9 +8,55 @@ import second from '../assets/two.jpg';
 const WorkDetail = () => {
     const audios = [ one, two ];
     const images = [ first, second ];
+    const [position, setPosition] = useState({
+        x: 0,
+        y: 0,
+    });
     const idParams = useParams();
 
     const audioRef = useRef();
+    const imageRef = useRef();
+    const cursorRef = useRef();
+
+    const handleNotHover = (e) => {
+        const { value } = e.target
+        e.target.style.zIndex = "0"
+        e.target.style.transition = "none"
+        console.log(e.target)
+    }
+
+    const handleHover = (e) => {
+        e.target.style.zIndex = "9999"
+        e.target.style.transition = "all 1500s"
+    }
+    
+    const getPosition = (e) => {
+        var x = e.clientX;
+        var y = e.clientY;
+        console.log(x,y)
+        setPosition({
+            x: x,
+            y: y,
+        });
+    }
+
+    const collapse = () => {
+        if(position.y < 80){
+            audioRef.current.style.top = "1%"
+        } else {
+            audioRef.current.style.top = "-10%"
+        }
+    }
+
+    const cursor = () => {
+        cursorRef.current.style.top = `${position.y}px`;
+        cursorRef.current.style.left =`${position.x}px`;
+    }
+
+    useEffect(() => {
+        collapse();
+        cursor();
+    })
 
     const init = () => {
         console.log(audios[idParams.workId]);
@@ -20,12 +66,16 @@ const WorkDetail = () => {
 
     useEffect(() => {
         init();
+        window.addEventListener('mousemove', getPosition, false);
+
+        return () => window.removeEventListener('mousemove', getPosition, false)
     }, [])
     
     return (
-        <div>
+        <div className ="container">
+            <div ref = { cursorRef } className = "cursor"></div>
             <h1>work { idParams.workId } page</h1>
-            <img src = { images[idParams.workId - 1]} alt = ""/>
+            <img ref = { imageRef } src = { images[idParams.workId - 1]} onMouseOut = { handleNotHover } onMouseOver = { handleHover } alt = ""/>
             <audio controls autoPlay ref = { audioRef } src = { audios[idParams.workId - 1] }></audio>
         </div>
     );
